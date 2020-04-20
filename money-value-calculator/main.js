40aabcac53b2c697075e0e0ed203f160905f162b
meduna femi <femimeduna@gmail.com>
2020-04-20 16:45:41 +0100

    handle input validation

--- money-value-calculator/main.js
+++ money-value-calculator/main.js
@@ -1,19 +1,18 @@
-// Hook up to the button
 const computeButton = document.getElementById("button");
+const warningZone = document.getElementById("warning-zone");
 
-// Add event Listener
+// Event Listeners
+warningZone.addEventListener("click", compoundInputWarning);
 computeButton.addEventListener("click", computeMoneyValue);
 
+//Compound Period Input Warning
 const compoundInputWarning = () => {
   let warning = document.getElementById("warning");
-
   warning.style.display = "block";
 };
-const warningZone = document.getElementById("warning-zone");
-warningZone.addEventListener("click", compoundInputWarning);
 
+// Calculations
 function computeMoneyValue() {
-  //Convert all values to numbers
   let principal = parseInt(document.getElementById("principal").value);
   let interestRate = parseInt(document.getElementById("interest-rate").value);
   let time = parseInt(document.getElementById("time").value);
@@ -22,6 +21,7 @@ function computeMoneyValue() {
     document.getElementById("compound-period").value
   );
 
+  // compute effective rate
   let effectiveRate = (1 + interestRate / 100) / (1 + inflation / 100) - 1;
   if (compoundPeriod > time) {
     return 0;
